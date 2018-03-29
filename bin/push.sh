7ac35aa78bd7e0588491878769c4f5ce921152c1
foobert <0x002a@gmail.com>
2018-03-29 19:27:51 +0200

    Parameterize push script

--- bin/push.sh
+++ bin/push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function main {
+  local image=$1
   local branch=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
   local tag=$(git show -s --format=%ct-%h HEAD)
 
@@ -11,13 +12,13 @@ function main {
 
   if [ "${branch}" = "master" ]
   then
-    docker push foobert/gc-siphon:latest
+    docker push "${image}:latest"
   else
     tag="${tag}-${branch}"
   fi
 
-  docker tag foobert/gc-siphon:latest "foobert/gc-siphon:${tag}"
-  docker push "foobert/gc-siphon:${tag}"
+  docker tag "${image}:latest" "${image}:${tag}"
+  docker push "${image}:${tag}"
 }
 
 set -e
